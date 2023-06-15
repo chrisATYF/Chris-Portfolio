5611ae130834ce3e0c3f4b76cbe227010dae5351
chrisATYF <36751474+chrisATYF@users.noreply.github.com>
2023-06-15 16:00:53 -0500

    Update supabase.js

--- src/supabase.js
+++ src/supabase.js
@@ -1,8 +1,8 @@
 import { createClient } from '@supabase/supabase-js'
 
-const { VITE_SUPABASE_URL, VITE_SUPABASE_ANON_KEY } = import.meta.env;
+const { VITE_PUBLIC_SUPABASE_URL, VITE_PUBLIC_SUPABASE_ANON_KEY } = import.meta.env;
 
-const supabaseUrl = VITE_SUPABASE_URL;
-const supabaseKey = VITE_SUPABASE_ANON_KEY;
+const supabaseUrl = VITE_PUBLIC_SUPABASE_URL;
+const supabaseKey = VITE_PUBLIC_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
