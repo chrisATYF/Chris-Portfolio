3c99b66ab33a80aa88cd02104d37605b4823a8c1
chrisATYF <36751474+chrisATYF@users.noreply.github.com>
2023-06-27 20:00:10 -0500

    Adding new nav
    This nav is mobile friendly and auto collapses menu upon click

--- src/App.vue
+++ src/App.vue
@@ -5,13 +5,37 @@ import Nav from "./components/Nav.vue"
 
 <template>
   <main>
-    <Nav></Nav>
+    <header class="header">
+      <Nav></Nav>
+    </header>
     <RouterView />
   </main>
 </template>
 
 <style>
+@import url("https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap");
+
+:root {
+  --white: #f9f9f9;
+  --black: #36383F;
+  --gray: #85888C;
+}
+
 body {
+  background-color: var(--white);
+  font-family: "Poppins", sans-serif;
   overflow: hidden;
+}
+
+ul {
+  list-style: none;
+}
+
+.header {
+  background-color: var(--black);
+  box-shadow: 1px 1px 5px 0px var(--gray);
+  position: sticky;
+  top: 0;
+  width: 100%;
 }
 </style>
--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,37 +1,146 @@
-<script>
-import { RouterLink } from 'vue-router';
+<script setup>
+import { ref } from 'vue';
+
+const isChecked = ref(false);
+
+const toggleInput = () => {
+    isChecked.value = false;
+};
 </script>
 
 <template>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <div class="container-fluid">
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
-                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li class="nav-item">
-                        <RouterLink class="nav-item" to="/">Home</RouterLink>
-                    </li>
-                    <li class="nav-item">
-                        <RouterLink class="nav-item" to="/portfolio">Portfolio</RouterLink>
-                    </li>
-                    <li class="nav-item">
-                        <RouterLink class="nav-item" to="/contact">Contact</RouterLink>
-                    </li>
-                </ul>
-            </div>
-        </div>
+    <a href="#" class="logo-link">
+        <img src="../../public/C.png" class="logo" />
+    </a>
+    <input v-model="isChecked" class="mobile-menu" id="mobile-menu" type="checkbox">
+    <label class="btn-text" for="mobile-menu"><span class="btn-line"></span></label>
+    <nav class="nav">
+        <ul class="menu">
+            <li>
+                <RouterLink class="nav-link" @click="toggleInput" to="/">Home</RouterLink>
+            </li>
+            <li>
+                <RouterLink class="nav-link" @click="toggleInput" to="/portfolio">Portfolio</RouterLink>
+            </li>
+            <li>
+                <RouterLink class="nav-link" @click="toggleInput" to="/contact">Contact</RouterLink>
+            </li>
+        </ul>
     </nav>
 </template>
 
 <style scoped>
-.nav-item {
-    color: black;
-    padding: 10px;
-    text-decoration: none;
-    margin: 0 6px;
-    letter-spacing: 2px;
+.logo {
+    height: 35px;
+    width: 35px;
+    vertical-align: unset;
+}
+
+.logo-link {
+    display: inline-block;
+    color: var(--white);
+    font-size: 60px;
+    margin-left: 20px;
+}
+
+.btn-text {
+    cursor: pointer;
+    float: right;
+    padding: 40px 20px;
+}
+
+.btn-line {
+    background: var(--white);
+    display: block;
+    height: 2px;
+    position: relative;
+    width: 24px;
+}
+
+.btn-line::before,
+.btn-line::after {
+    background: var(--white);
+    content: '';
+    display: block;
+    height: 100%;
+    position: absolute;
+    transition: all .2s ease-out;
+    width: 100%;
+}
+
+.btn-line::before {
+    top: 5px;
+}
+
+.btn-line::after {
+    top: -5px;
+}
+
+.mobile-menu {
+    display: none;
+}
+
+.mobile-menu:checked~nav {
+    max-height: 100%;
+}
+
+.mobile-menu:checked~.btn-text .btn-line {
+    background: transparent;
+}
+
+.mobile-menu:checked~.btn-text .btn-line::before {
+    transform: rotate(-45deg);
+    top: 0;
+}
+
+.mobile-menu:checked~.btn-text .btn-line::after {
+    transform: rotate(45deg);
+    top: 0;
+}
+
+.nav {
+    /* New Add */
+    display: block;
+    width: 100%;
+    height: 100%;
+    /* position: fixed; */
+    background-color: var(--black);
+    overflow: hidden;
+    max-height: 0;
+    /* transition: max-height .5s ease-out; */
+}
+
+.menu a {
+    display: block;
+    padding: 30px;
+    color: var(--white);
+}
+
+.menu a:hover {
+    background-color: var(--gray);
+}
+
+@media (min-width: 768px) {
+    .nav {
+        max-height: none;
+        top: 0;
+        position: relative;
+        float: right;
+        width: fit-content;
+        background-color: transparent;
+    }
+
+    .menu li {
+        float: left;
+    }
+
+    .menu a:hover {
+        background-color: transparent;
+        color: var(--gray);
+    }
+
+    .btn-text {
+        display: none;
+    }
 }
 </style>
